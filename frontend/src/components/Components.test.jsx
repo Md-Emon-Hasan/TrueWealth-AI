97f2c6e6272e84eade13203e4dc7d36e5f83f15f
md-emon-hasan <iconicemon01@gmail.com>
2026-02-16 16:58:20 +0600

    eslist errors

--- frontend/src/components/Components.test.jsx
+++ frontend/src/components/Components.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { expect, test, vi } from 'vitest';
+import { expect, test } from 'vitest';
 import Sidebar from './Sidebar';
 import StatusBar from './StatusBar';
 import React from 'react';
